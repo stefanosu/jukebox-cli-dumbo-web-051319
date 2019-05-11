6914d176861a931bea3bd76e5460cec1dc042e68
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-11 18:59:12 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -23,13 +23,10 @@ def help
 end
 
 
-
-def list(my_songs)
   def list(my_songs)
   my_songs.each_with_index do |song, index|
     puts "#{index+1}. #{song}"
   end
-end
 
   
   #this method is different! Collect the keys of the my_songs hash and 
