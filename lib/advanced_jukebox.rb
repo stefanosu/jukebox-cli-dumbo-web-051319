495ccda87bca5014b3fb3537f155d9fd7e930d88
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-11 19:00:54 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -57,5 +57,27 @@ end #this method is the same as in jukebox.rb
 
 
 def run(my_songs)
-  #this method is the same as in jukebox.rb
+  def run(songs)
+  #help
+  command = "" 
+  while command  
+  puts "Please enter a command:"
+  command = gets.downcase.strip  
+  case command 
+    when 'list'
+      list(songs)
+      when 'play'
+        list(songs)
+        play(songs)
+      when 'help'
+        help 
+      when 'exit'
+        exit_jukebox
+        break 
+      else 
+        help 
+      end 
+    end 
+  end 
+  run(songs)#this method is the same as in jukebox.rb
 end
