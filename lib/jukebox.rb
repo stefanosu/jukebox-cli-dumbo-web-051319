9daa54ab115772673f49bbd494fb5a0dada45bb5
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-11 19:02:44 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -9,3 +9,16 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+
+def help
+  help = <<- HELP
+I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program
+HELP
+  puts  
+end
+  
+  
